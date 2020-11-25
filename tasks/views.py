4a55a540487be70c159bbb008940f1b2cbef4d07
luisboss18 <73471985+luisboss18@users.noreply.github.com>
2020-11-25 13:34:27 +0000

    25-11-20

---
+++ tasks/views.py
@@ -0,0 +1,11 @@
+from django.shortcuts import render
+from  django.http import HttpResponse
+
+def helloWorld (request):
+    return HttpResponse('Hello World!')
+
+def tasklist(request):
+    return render(request, 'tasks/list.html')
+
+def yourName(request, name):
+    return render(request, 'tasks(yourname.html', {'name':name})
